b8c50765749527d2f2a62244782bc37f8cf050b7
liseningg <64001103+liseningg@users.noreply.github.com>
2020-04-23 14:24:04 +0800

    add code

--- 剑指offer/023-二叉搜索树的后序遍历序列/code.py
+++ 剑指offer/023-二叉搜索树的后序遍历序列/code.py
@@ -15,3 +15,28 @@ def VerifySquenceOfBST(sequence):
     if not sequence: return False
     return helper(sequence)
 print(VerifySquenceOfBST([7,4,6,5]))
+
+我的解法
+    def VerifySquenceOfBST(self, sequence):
+        # write code here
+        if sequence==None or len(sequence)==0:
+            return False
+        length=len(sequence)
+        root=sequence[length-1]
+        # 在二叉搜索 树中 左子树节点小于根节点
+        for i in range(length):
+            if sequence[i]>root:
+                break
+        # 二叉搜索树中右子树的节点都大于根节点
+        for j  in range(i,length):
+            if sequence[j]<root:
+                return False
+        # 判断左子树是否为二叉树
+        left=True
+        if  i>0:
+            left=self.VerifySquenceOfBST(sequence[0:i])
+        # 判断 右子树是否为二叉树
+        right=True
+        if i<length-1:
+            right=self.VerifySquenceOfBST(sequence[i:-1])
+        return left and right
