48c78d7c300c9861c8dbd4585698c4f8d5157747
liseningg <64001103+liseningg@users.noreply.github.com>
2020-04-23 14:13:20 +0800

    add code

--- 剑指offer/022-从上往下打印二叉树/code.py
+++ 剑指offer/022-从上往下打印二叉树/code.py
@@ -14,3 +14,21 @@ def PrintFromTopToBottom(root):
             if node.right:
                 queue.append(node.right)
     return res
+
+我的解法
+
+    def PrintFromTopToBottom(self, root):
+        # write code here 
+        if not root:
+            return []
+        queue,result = [],[]
+        queue.append(root)
+        while len(queue) > 0:
+            node = queue.pop(0)
+            result.append(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        return result
+    
