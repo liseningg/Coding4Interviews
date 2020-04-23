8be87678216870013f13a922fa09bcf62bd9d621
liseningg <64001103+liseningg@users.noreply.github.com>
2020-04-23 14:06:44 +0800

    add code

--- 剑指offer/018-二叉树的镜像/code.py
+++ 剑指offer/018-二叉树的镜像/code.py
@@ -8,3 +8,13 @@ def Mirror(root):
     root.right = Mirror(root.left)
     root.left = tmp
     return root
+
+我的解法
+
+    def Mirror(self, root):
+        # write code here
+        if root != None:
+            root.left,root.right = root.right,root.left
+            self.Mirror(root.left)
+            self.Mirror(root.right)
+            
