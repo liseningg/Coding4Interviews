2d4c230b24250e48bc1cddd6f41570d1109be681
liseningg <64001103+liseningg@users.noreply.github.com>
2020-04-23 21:54:48 +0800

    add code

--- 剑指offer/057-二叉树的下一个结点/code.py
+++ 剑指offer/057-二叉树的下一个结点/code.py
@@ -19,3 +19,37 @@ class Solution:
         while pNode.next and pNode.next.right == pNode:
             pNode = pNode.next
         return pNode.next
+    
+我的解法：
+1.
+class Solution:
+    def GetNext(self, pNode):
+        # write code here
+        if pNode.right:#有右子树
+            p=pNode.right
+            while p.left:
+                p=p.left
+            return p
+        while pNode.next:#无右子树，则找第一个当前节点是父节点左孩子的节点
+            if(pNode.next.left==pNode):
+                return pNode.next
+            pNode = pNode.next#沿着父节点向上遍历
+        return  #到了根节点仍没找到，则返回空
+    
+2.
+class Solution:
+    def GetNext(self, pNode):
+        # 1.寻找右子树，如果存在就一直找到右子树的最左边，就是下一个节点
+        # 2.没有右子树，就寻找他的父节点，一直找到它是父节点的左子树，打印父节点
+        if pNode.right:
+            tmpNode = pNode.right
+            while tmpNode.left:
+                tmpNode = tmpNode.left
+            return tmpNode
+        else:
+            tmpNode = pNode
+            while tmpNode.next:
+                if tmpNode.next.left == tmpNode:
+                    return tmpNode.next
+                tmpNode = tmpNode.next
+            return None
