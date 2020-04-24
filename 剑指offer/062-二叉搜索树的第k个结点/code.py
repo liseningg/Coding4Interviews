367766493570245bf01c4d1d8502bf6df9904891
liseningg <64001103+liseningg@users.noreply.github.com>
2020-04-24 14:18:46 +0800

    add code

--- 剑指offer/062-二叉搜索树的第k个结点/code.py
+++ 剑指offer/062-二叉搜索树的第k个结点/code.py
@@ -15,3 +15,20 @@ class Solution:
             if k == 0:
                 return pRoot
             pRoot = pRoot.right
+            
+递归法
+class Solution:
+    # 返回对应节点TreeNode
+    def KthNode(self, pRoot, k):
+        # write code here
+        res = []
+        def preorder(pRoot):
+            if not pRoot:
+                return None
+            preorder(pRoot.left)
+            res.append(pRoot)
+            preorder(pRoot.right)
+        preorder(pRoot)
+        if len(res) < k or k <1:
+            return None
+        return res[k-1]
