02a3e05d2105b058282009617105b4c4b9528388
liseningg <64001103+liseningg@users.noreply.github.com>
2020-04-24 17:38:26 +0800

    add code

--- 剑指offer/021-栈的压入、弹出序列/code.py
+++ 剑指offer/021-栈的压入、弹出序列/code.py
@@ -14,3 +14,20 @@ def IsPopOrder(pushV, popV):
     
 
 print(IsPopOrder([1, 2, 3, 4, 5], [4,3,5,1,2]))
+
+我的解法：
+class Solution:
+    def IsPopOrder(self, pushV, popV):
+        # write code here
+        if len(pushV) != len(popV) or not pushV:
+            return False
+        stack = []
+        for i in pushV:
+            stack.append(i)
+            while len(stack) and stack[-1] == popV[0]: 
+#len(stack)表示对stack剩下的值与popV进行比较，当len（stack）=0时，while停止循环
+                stack.pop()
+                popV.pop(0)
+        if stack:
+            return False
+        return True
