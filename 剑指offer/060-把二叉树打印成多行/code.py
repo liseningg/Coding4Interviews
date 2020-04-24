ef4ca147c15a5b37b5caca325026b4d3b8cb5715
liseningg <64001103+liseningg@users.noreply.github.com>
2020-04-24 10:36:29 +0800

    add code

--- 剑指offer/060-把二叉树打印成多行/code.py
+++ 剑指offer/060-把二叉树打印成多行/code.py
@@ -15,4 +15,40 @@ class Solution:
                 if node.right: queue.append(node.right)
             res.append(temp)
         return res
+    
+    
+我的解法
+class Solution:
+    # 返回二维列表[[1,2],[4,5]]
+    def Print(self, pRoot):
+        if pRoot == None:
+            return []
+        queue1 = [pRoot]
+        queue2 = []
+        ret = []
+
+        while queue1 or queue2:
+            if queue1:
+                tmpRet = []
+                while queue1:
+                    tmpNode = queue1[0]
+                    tmpRet.append(tmpNode.val)
+                    queue1.pop(0)
+                    if tmpNode.left:
+                        queue2.append(tmpNode.left)
+                    if tmpNode.right:
+                        queue2.append(tmpNode.right)
+                ret.append(tmpRet)
+            if queue2:
+                tmpRet = []
+                while queue2:
+                    tmpNode = queue2[0]
+                    tmpRet.append(tmpNode.val)
+                    queue2.pop(0)
+                    if tmpNode.left:
+                        queue1.append(tmpNode.left)
+                    if tmpNode.right:
+                        queue1.append(tmpNode.right)
+                ret.append(tmpRet)
+        return ret
                 
