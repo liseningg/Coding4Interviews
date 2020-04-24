03e6bc7ab4a4d8393775346d835b44d9f4b33c8f
liseningg <64001103+liseningg@users.noreply.github.com>
2020-04-24 17:20:34 +0800

    add code

--- 剑指offer/005-用两个栈实现队列/code.py
+++ 剑指offer/005-用两个栈实现队列/code.py
@@ -14,3 +14,19 @@ class CQueue(object):
         if len(self.stack2) == 0: 
             return -1
         return self.stack2.pop(-1)
+
+我的解法
+class Solution:
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+    def push(self, node):
+        self.stack1.append(node)
+        # write code here 
+    def pop(self):
+        if self.stack2 == []:
+            while self.stack1:
+                a = self.stack1.pop()
+                self.stack2.append(a)
+        return self.stack2.pop()
+        # return xx
