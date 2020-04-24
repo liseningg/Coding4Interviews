2f56c60eb030ff197e3e6a83825b561212292f01
liseningg <64001103+liseningg@users.noreply.github.com>
2020-04-24 18:19:52 +0800

    add code

--- 剑指offer/044-翻转单词顺序列/code.py
+++ 剑指offer/044-翻转单词顺序列/code.py
@@ -8,7 +8,21 @@ class Solution:
 print(Solution().ReverseSentence("I am a student."))
 
 我的解法
+1
 class Solution:
     def ReverseSentence(self, s):
         # write code here
         return " ".join(s.split(" ")[::-1])
+
+2
+class Solution:
+    def ReverseSentence(self, s):
+        # write code here
+        alist = s.split(" ")
+        stacklist = []
+        for i in alist:
+            stacklist.append(i)
+        result = []
+        for _ in range(len(stacklist)):
+            result.append(stacklist.pop())
+        return " ".join(result)
