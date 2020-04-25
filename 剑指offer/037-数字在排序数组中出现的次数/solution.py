031836e582ddf44d926e1697002c5c5b7f38d6d9
liseningg <64001103+liseningg@users.noreply.github.com>
2020-04-25 09:49:14 +0800

    add code

--- 剑指offer/037-数字在排序数组中出现的次数/solution.py
+++ 剑指offer/037-数字在排序数组中出现的次数/solution.py
@@ -15,3 +15,70 @@ def GetNumberOfK(data, k):
     return biSearch(data, k+0.5) - biSearch(data, k-0.5)
 
 print(GetNumberOfK([3,3,3, 4],3))
+
+
+我的解法
+1（易理解）
+# -*- coding:utf-8 -*-
+class Solution:
+    def GetNumberOfK(self, data, k):
+        # write code here
+        #left  左边<k  右边>=k
+        #right 左边<=k 右边>k
+        #right-left+1
+        if not data:
+            return 0
+        l,r = 0,len(data)-1
+        while l<r:
+            mid = (l+r) /2
+            if data[mid] < k:
+                l = mid +1
+            else:
+                r = mid
+        if data[l] != k:
+            return 0
+        left = l
+        l,r = 0,len(data)-1
+
+        while l<r:
+            mid = (l+r+1)/2
+            if data[mid] <= k:
+                l = mid
+            else:
+                r = mid -1
+        right = r
+        return right-left+1
+    
+  
+2
+# -*- coding:utf-8 -*-
+class Solution:
+    def GetNumberOfK(self, data, k):
+        # write code here
+        if not data:  return 0
+ 
+        # 二分查找，找出第一次出现的下标
+        def get_first(data, k):
+            l, r = 0, len(data) - 1
+            while l <= r:
+                m = l + (r - l)//2
+                if data[m] < k:
+                    l = m + 1
+                else:
+                    r = m - 1
+            return l
+ 
+        # 找出最后一次出现的下标
+        def get_last(data, k):
+            l, r = 0, len(data) - 1
+            while l <= r:
+                m = l + (r - l)//2
+                if data[m] <= k:
+                    l = m + 1
+                else:
+                    r = m - 1
+            return r
+         
+        first_index = get_first(data, k)
+        last_index = get_last(data, k)
+        return last_index - first_index + 1
