6cedb2a0b9cff6aa52587d7bc9ee0b6ff2a7d16a
liseningg <64001103+liseningg@users.noreply.github.com>
2020-04-27 15:56:40 +0800

    Update code.py

--- 剑指offer/030-连续子数组的最大和/code.py
+++ 剑指offer/030-连续子数组的最大和/code.py
@@ -35,3 +35,26 @@ class Solution:
                 maxNum = tmpNum
         # 最终返回最大值
         return maxNum
+
+# 递归
+该题目思路:
+dp[i]表示以元素array[i]结尾的最大连续子数组和.
+以[-2,-3,4,-1,-2,1,5,-3]为例
+可以发现,
+dp[0] = -2
+dp[1] = -3
+dp[2] = 4
+dp[3] = 3
+以此类推,会发现
+dp[i] = max{dp[i-1]+array[i],array[i]}.
+
+# -*- coding:utf-8 -*-
+class Solution:
+    def FindGreatestSumOfSubArray(self, array):
+        # write code here
+        n = len(array)
+        dp = [ i for i in array]
+        for i in range(1,n):
+            dp[i] = max(dp[i-1]+array[i],array[i])
+         
+        return max(dp)
