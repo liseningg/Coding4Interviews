3b8dd52636eea8afc022ffa52f25a368c048b4b2
liseningg <64001103+liseningg@users.noreply.github.com>
2020-04-27 14:08:05 +0800

    add code

--- 剑指offer/029-最小的K个数/code.py
+++ 剑指offer/029-最小的K个数/code.py
@@ -30,3 +30,66 @@ def GetLeastNumbers_Solution(tinput, k):
 
 
 print(GetLeastNumbers_Solution([4,5,1,6,2,7,3,8],4))
+
+
+# 我的解法
+# 堆排序
+class Solution:
+    def GetLeastNumbers_Solution(self, tinput, k):
+        # 第一种
+        # if len(tinput) < k:
+        #     return []
+        # ret = sorted(tinput)
+        # return ret[:k]
+
+        # 第二种,使用最大堆
+        # 创建或者是插入最大堆
+        def createMaxHeap(num):
+            maxHeap.append(num)
+            currentIndex = len(maxHeap) - 1
+            while currentIndex != 0:
+                parentIndex = (currentIndex - 1) >> 1
+                if maxHeap[parentIndex] < maxHeap[currentIndex]:
+                    maxHeap[parentIndex], maxHeap[currentIndex] = maxHeap[currentIndex], maxHeap[parentIndex]
+                    currentIndex = parentIndex
+                else:
+                    break
+
+        # 调整最大堆，头节点发生改变
+        def adjustMaxHeap(num):
+            if num < maxHeap[0]:
+                maxHeap[0] = num
+
+            maxHeapLen = len(maxHeap)
+            index = 0
+            while index < maxHeapLen:
+                leftIndex = index * 2 + 1
+                rightIndex = index * 2 + 2
+                if rightIndex < maxHeapLen:
+                    if maxHeap[rightIndex] < maxHeap[leftIndex]:
+                        largerIndex = leftIndex
+                    else:
+                        largerIndex = rightIndex
+                elif leftIndex < maxHeapLen:
+                    largerIndex = leftIndex
+                else:
+                    break
+
+                if maxHeap[index] < maxHeap[largerIndex]:
+                    maxHeap[index], maxHeap[largerIndex] = maxHeap[largerIndex], maxHeap[index]
+                index = largerIndex
+
+        maxHeap = []
+        tinputLen = len(tinput)
+
+        if tinputLen < k or k <= 0:
+            return []
+
+        for i in range(tinputLen):
+            if i < k:
+                createMaxHeap(tinput[i])
+            else:
+                adjustMaxHeap(tinput[i])
+
+        return sorted(maxHeap)
+
