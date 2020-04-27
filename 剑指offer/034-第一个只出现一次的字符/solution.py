35aac95839c11eed759f75a65eff0661d723a7ae
liseningg <64001103+liseningg@users.noreply.github.com>
2020-04-27 14:42:09 +0800

    add code

--- 剑指offer/034-第一个只出现一次的字符/solution.py
+++ 剑指offer/034-第一个只出现一次的字符/solution.py
@@ -9,3 +9,34 @@ def FirstNotRepeatingChar(s):
     return -1
 
 print(FirstNotRepeatingChar('abac'))
+
+
+#方法一：利用数组自己建立个哈希表
+class Solution:
+    def FirstNotRepeatingChar(self, s):
+        # write code here
+        if s==' ':
+            return -1
+        hashtable=[0]*256
+        for i in s:
+            hashtable[ord(i)] += 1
+        for i in s:
+            if hashtable[ord(i)]==1:
+                return s.index(i)
+        return -1
+ 
+#方法二：利用python自带的字典
+class Solution:
+    def FirstNotRepeatingChar(self, s):
+        # write code here
+        if s==' ':
+            return -1
+        d=dict()
+        #第一次遍历字符串，将每个字符的次数存入字典
+        for i in s:    
+            d[i]=d.get(i,0)+1
+        #第二次遍历字符串，检查每个字符出现的次数
+        for i in s:
+            if d[i]==1:   #O(1)
+                return s.index(i)
+        return -1
