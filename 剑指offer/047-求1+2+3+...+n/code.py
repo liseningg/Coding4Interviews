99084ab1209012f7c052a99cb4298208101597ad
liseningg <64001103+liseningg@users.noreply.github.com>
2020-04-28 21:56:28 +0800

    add code

--- 剑指offer/047-求1+2+3+...+n/code.py
+++ 剑指offer/047-求1+2+3+...+n/code.py
@@ -12,3 +12,17 @@ class Solution:
 
 
 print(Solution().Sum_Solution(10))
+
+我的解法
+不能用乘除法、循环和条件判断语句，就用位运算代替乘除法，用递归代替循环，用短路替代条件判断（不过此题用不上乘除法）。
+
+从n开始往回加；
+使用ans and，与运算的短路原理，若ans为0了就跳出递归；
+使用递归代替循环做加法。
+# -*- coding:utf-8 -*-
+class Solution:
+    def Sum_Solution(self, n):
+        ans = n
+        tmp = ans and self.Sum_Solution(n - 1)
+        ans += tmp
+        return ans
