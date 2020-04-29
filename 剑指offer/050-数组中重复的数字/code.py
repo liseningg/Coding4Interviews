46e9c11b63bf6968676f3233c203dc7af8a650fb
liseningg <64001103+liseningg@users.noreply.github.com>
2020-04-29 10:11:28 +0800

    add code

--- 剑指offer/050-数组中重复的数字/code.py
+++ 剑指offer/050-数组中重复的数字/code.py
@@ -11,3 +11,20 @@ class Solution:
             else:
                 map[n] = 1
         return False
+    
+我的解法
+class Solution:
+    # 这里要特别注意~找到任意重复的一个值并赋值到duplication[0]
+    # 函数返回True/False
+    def duplicate(self, numbers, duplication):
+        # write code here
+        if len(numbers)<2:
+            return False
+        dic = {}
+        for i in numbers:
+            if i in dic:
+                duplication[0] = i
+                return True
+            else:
+                dic[i] = 1
+        return False
