7e5ea654a970c0698313d20a2171cbf3ad8de966
liseningg <64001103+liseningg@users.noreply.github.com>
2020-04-29 11:09:38 +0800

    Update code.py

--- 剑指offer/051-构建乘积数组/code.py
+++ 剑指offer/051-构建乘积数组/code.py
@@ -3,11 +3,11 @@ class Solution:
         # write code here
         B = [1] * len(A)
         temp = 1
-        for i in range(1, len(A)):
+        for i in range(1, len(A)):   #这里range的范围要看B是从哪里开始的
             temp *= A[i-1]
             B[i] *= temp
         temp = 1
-        for i in range(len(A)-2, -1, -1):
+        for i in range(len(A)-2, -1, -1):  #B从n-2开始，0结束，A根据B来取对应的值
             temp *= A[i+1]
             B[i] *= temp
         return B
