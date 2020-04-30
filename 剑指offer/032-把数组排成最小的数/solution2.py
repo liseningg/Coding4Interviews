268a05eebb7cd78f15d4757f9c8f9fd8335846d7
liseningg <64001103+liseningg@users.noreply.github.com>
2020-04-30 10:53:38 +0800

    add code

--- 剑指offer/032-把数组排成最小的数/solution2.py
+++ 剑指offer/032-把数组排成最小的数/solution2.py
@@ -17,4 +17,19 @@ def PrintMinNumber(numbers):
 
 print(PrintMinNumber([32, 3, 321]))
     
-    
+我的解法
+空数组返回空串；
+将数组中的数字转化为字符串；
+字符串拼接后的字典排序；
+拼接返回；
+# -*- coding:utf-8 -*-
+class Solution:
+    def PrintMinNumber(self, numbers):
+        if not numbers: return ''
+
+        numbers = list(map(str, numbers))
+        #cmp参数其中的规律就是：两两比较，如果返回为正，则交换两者的位置，
+        #cmp（x，y） x>y 1 x<y -1 x=y 0
+        numbers.sort(cmp=lambda x, y: cmp(x + y, y + x))
+        return ''.join(numbers).lstrip('0') or '0'
+
