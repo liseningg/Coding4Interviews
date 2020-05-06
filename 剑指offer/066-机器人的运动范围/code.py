cb0ed8b12f6616dcecfdb2ab527cd18d0712c3d3
liseningg <64001103+liseningg@users.noreply.github.com>
2020-05-06 11:27:26 +0800

    add code

--- 剑指offer/066-机器人的运动范围/code.py
+++ 剑指offer/066-机器人的运动范围/code.py
@@ -38,3 +38,39 @@ def BFS(array, i, j, threshold, visited):
     res += BFS(array, i, j+1, threshold, visited)
     res += BFS(array, i, j-1, threshold, visited)
     return res
+
+我的解法
+# -*- coding:utf-8 -*-
+class Solution:
+    def __init__(self):
+        self.count = 0
+ 
+    def movingCount(self, threshold, rows, cols):
+        # 标志是否走过，没走过的标0，走过标1
+        flag = [[0 for i in range(cols)] for j in range(rows)]
+        self.find_way(flag, 0, 0, threshold)
+        return self.count
+ 
+    def find_way(self, flag, i, j, threshold):
+        # 1. 越界返回
+        if i < 0 or j < 0 or i >= len(flag) or j >= len(flag[0]):
+            return
+ 
+        # 2. 超过阈值返回
+        tmp_i = list(map(int, list(str(i))))
+        tmp_j = list(map(int, list(str(j))))
+        if sum(tmp_i) + sum(tmp_j) > threshold:
+            return
+ 
+        # 已经走过的位置，返回
+        if flag[i][j]: return
+ 
+        # 此位置没走过，计数加一
+        flag[i][j] = 1
+        self.count += 1
+ 
+        # 向四个方向继续走
+        self.find_way(flag, i + 1, j, threshold)
+        self.find_way(flag, i - 1, j, threshold)
+        self.find_way(flag, i, j + 1, threshold)
+        self.find_way(flag, i, j - 1, threshold)
