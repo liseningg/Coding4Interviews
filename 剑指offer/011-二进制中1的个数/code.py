58b4eb5eb39dd7fec1bad8b10629a87a2e48fb83
liseningg <64001103+liseningg@users.noreply.github.com>
2020-08-09 15:17:53 +0800

    Update code.py

--- 剑指offer/011-二进制中1的个数/code.py
+++ 剑指offer/011-二进制中1的个数/code.py
@@ -18,3 +18,16 @@ print(NumberOf1(-3))
 #     def NumberOf1(self, n):
 #         # write code here
 #         return bin(n).replace("0b","").count("1") if n>=0 else bin(2**32+n).replace("0b","").count("1")
+
+
+
+class Solution:
+    def NumberOf1(self, n):
+        # write code here
+        count = 0
+        if n < 0:
+            n = n & 0xffffffff     #对于负数，最高位为1，而负数在计算机是以补码存在的，往右移，符号位不变，符号位1往右移，最终可能会出现全1的情况，导致死循环。与0xffffffff相与，就可以消除负数的影响
+        while n: 
+            count += 1
+            n = (n - 1) & n
+        return count
